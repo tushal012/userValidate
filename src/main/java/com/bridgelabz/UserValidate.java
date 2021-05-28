11e7691f4f8ad0be0c6f62bb5fb83d36365efe98
tushal012 <tushalkashyap11@gmail.com>
2021-05-28 20:49:39 +0530

    ADDED: password vlidation with minimum 8 character

--- src/main/java/com/bridgelabz/UserValidate.java
+++ src/main/java/com/bridgelabz/UserValidate.java
@@ -37,6 +37,16 @@ public class UserValidate {
 
         return m.matches();
     }
+    public static boolean isValidPassword(String password) {
+        String regexPassword = "^[a-z A-z]{8,}$";
+        Pattern p = Pattern.compile(regexPassword);
+        if ( password == null ){
+            return false;
+        }
+        Matcher m = p.matcher(password);
+
+        return m.matches();
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the username validation program.");
@@ -57,6 +67,10 @@ public class UserValidate {
         System.out.print("Please enter the your mobile with country code 91: ");
         String mobileNo = input.nextLine();
         System.out.println(isValidMobileNum(mobileNo));
+
+        System.out.print("please enter the password: ");
+        String userPassword = input.nextLine();
+        System.out.println(isValidPassword(userPassword));
     }
 
 
