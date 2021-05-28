3ed66eadcdd83e5c335a90ad87e80ca721c2db4e
tushal012 <tushalkashyap11@gmail.com>
2021-05-28 15:59:43 +0530

    ADDED: added last Name

--- src/main/java/com/bridgelabz/UserValidate.java
+++ src/main/java/com/bridgelabz/UserValidate.java
@@ -5,9 +5,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidate {
-    public static boolean isValidUsername(String name) {
-        String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
-        Pattern p = Pattern.compile(regex);
+    public static boolean isValidFirstName(String name) {
+        String regexFirstName = "^[A-Z]{1}[a-z A-Z]{2,}$";
+        Pattern p = Pattern.compile(regexFirstName);
         if (name == null) {
             return false;
         }
@@ -19,10 +19,15 @@ public class UserValidate {
         System.out.println("welcome to username validation program");
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Please enter the your username: ");
-        String userName = input.nextLine();
+        System.out.print("Please enter the your First Name: ");
+        String firstName = input.nextLine();
+        System.out.println(isValidFirstName(firstName));
 
-        System.out.println(isValidUsername(userName));
+        System.out.print("Please enter the your last Name: ");
+        String lastName = input.nextLine();
+        System.out.println(isValidFirstName(lastName));
     }
+
+
 }
 
