8f4510cb837ef458b679524ecfcb11e228a69373
tushal012 <tushalkashyap11@gmail.com>
2021-05-28 15:50:51 +0530

    ADDED: added first Name

--- src/main/java/com/bridgelabz/UserValidate.java
+++ src/main/java/com/bridgelabz/UserValidate.java
@@ -1,8 +1,28 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserValidate {
-    public static void main(String[] args) {
-        System.out.println("welcome to username validation program");
+    public static boolean isValidUsername(String name) {
+        String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        if (name == null) {
+            return false;
+        }
+        Matcher m = p.matcher(name);
+        return m.matches();
 
     }
+    public static void main(String[] args) {
+        System.out.println("welcome to username validation program");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the your username: ");
+        String userName = input.nextLine();
+
+        System.out.println(isValidUsername(userName));
+    }
 }
+
