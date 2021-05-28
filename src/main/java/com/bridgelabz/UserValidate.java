bc64bfd99262b45c6c4acde78cdd8f35abe8e230
tushal012 <tushalkashyap11@gmail.com>
2021-05-28 15:40:15 +0530

    Added: welcome message

---
+++ src/main/java/com/bridgelabz/UserValidate.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class UserValidate {
+    public static void main(String[] args) {
+        System.out.println("welcome to username validation program");
+
+    }
+}
