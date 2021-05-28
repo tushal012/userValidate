7f919d9fe9ddeb3c3660b0c78bd116abd6affa6f
tushal012 <tushalkashyap11@gmail.com>
2021-05-28 20:37:07 +0530

    ADDED: email address validation

--- src/main/java/com/bridgelabz/UserValidate.java
+++ src/main/java/com/bridgelabz/UserValidate.java
@@ -14,9 +14,22 @@ public class UserValidate {
         Matcher m = p.matcher(name);
         return m.matches();
 
+
     }
+    public static boolean isValidEmail(String email) {
+//
+        String regexEmail = "^[0-9 A-Z a-z]+(([._+-]*)[0-9A-Za-z]+)*@[0-9 A-Z a-z]+.[a-z]{2,4}([.][a-z]{2,3})*$";
+        Pattern p = Pattern.compile(regexEmail);
+        if (email == null) {
+            return false;
+        }
+        Matcher m = p.matcher(email);
+        return m.matches();
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("welcome to username validation program");
+        System.out.println("Welcome to the username validation program.");
         Scanner input = new Scanner(System.in);
 
         System.out.print("Please enter the your First Name: ");
@@ -26,7 +39,12 @@ public class UserValidate {
         System.out.print("Please enter the your last Name: ");
         String lastName = input.nextLine();
         System.out.println(isValidFirstName(lastName));
+
+        System.out.print("Please enter the your Email ID: ");
+        String emailID = input.nextLine();
+        System.out.println(isValidEmail(emailID));
     }
+
 
 
 }
